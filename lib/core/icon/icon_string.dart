65be617e417e654cd26c6f80c86fff2d45866b11
Vinay_Sharma <vinaysharma31.12.1999@gmail.com>
2025-03-29 16:04:52 +0530

    assets, string file

---
+++ lib/core/icon/icon_string.dart
@@ -0,0 +1,7 @@
+const String kBackArrowIcon = "assets/icons/back_arrow.png";
+const String kBookmarkIcon = "assets/icons/bookmark.png";
+const String kCaretDownIcon = "assets/icons/caret_down.png";
+const String kPencilIcon = "assets/icons/pencil.png";
+const String kArrowDownIcon = "assets/icons/arrow_down.png";
+const String kArrowUpIcon = "assets/icons/arrow_up.png";
+const String kLogoIcon = "assets/icons/logo.webp";
