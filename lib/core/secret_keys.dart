a398e658a8f240dfcc183bfd6c8c76dbbefcba77
Vinay_Sharma <vinaysharma31.12.1999@gmail.com>
2025-03-29 22:14:21 +0530

    secret_keys consts

---
+++ lib/core/secret_keys.dart
@@ -0,0 +1,6 @@
+// ignore_for_file: constant_identifier_names, non_constant_identifier_names
+
+class SecretKeys {
+  static const String SUPABASE_URL = String.fromEnvironment("SUPABASE_URL");
+  static const String SUPABASE_KEY = String.fromEnvironment("SUPABASE_KEY");
+}
